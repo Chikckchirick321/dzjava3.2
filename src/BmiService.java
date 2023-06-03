724dc462d3cceea83cd96664f9c7604c8cb8a84f
Memotest <mov3st@yandex.ru>
2023-06-03 12:13:51 +0300

    Main&Service Update

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,8 @@
 public class BmiService {
 
-    public int calculate( float height, int weight)
-    {
+    public int calculate(float height, int weight) {
         //Индекс Массы Тела рассчитывается по формуле: ИМТ = вес (кг) / рост (м)2.
 
-        return (int)( weight / ( height * height ) );
+        return (int) (weight / (height * height));
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         BmiService service = new BmiService();
 
         // int weight = 98;
         // float height = 1.87F;
 
 
-        int bmi = service.calculate(1.87F,98);
+        int bmi = service.calculate(1.87F, 98);
         System.out.println(bmi);
-
-
     }
 }
